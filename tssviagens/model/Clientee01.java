fcc6dc62faaaff4d6379f8a27012a9497623e0f4
thamysantana <108750766+thamysantana@users.noreply.github.com>
2022-11-13 21:59:52 -0300

    Update Clientee01.java

--- tssviagens/model/Clientee01.java
+++ tssviagens/model/Clientee01.java
@@ -5,22 +5,15 @@ import java.util.Date;
 public class Clientee01 {
 
 	//atributos
-	private int id_cpf;
-	private String nome;
+	private int id_cliente;
 	private String email;
-	private String telefone;
-	private Date dtNasc;
-	public int getId_cpf() {
-		return id_cpf;
+	private String senha;
+	public int getId_cliente() {
+		return id_cliente;
 	}
-	public void setId_cpf(int id_cpf) {
-		this.id_cpf = id_cpf;
-	}
-	public String getNome() {
-		return nome;
-	}
-	public void setNome(String nome) {
-		this.nome = nome;
+	public void setId_cliente(int id_cliente) {
+		this.id_cliente = id_cliente;
+
 	}
 	public String getEmail() {
 		return email;
@@ -28,16 +21,12 @@ public class Clientee01 {
 	public void setEmail(String email) {
 		this.email = email;
 	}
-	public String getTelefone() {
-		return telefone;
+
+	public String getSenha() {
+		return senha;
 	}
-	public void setTelefone(String telefone) {
-		this.telefone = telefone;
+	public void setSenha(String senha) {
+		this.senha = senha;
 	}
-	public Date getDtNasc() {
-		return dtNasc;
-	}
-	public void setDtNasc(Date dtNasc) {
-		this.dtNasc = dtNasc;
-	}
+
 }
