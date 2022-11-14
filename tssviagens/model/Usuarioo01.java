ca253b723ea9b5bc2274cd41fe34d75684a9aff6
thamysantana <108750766+thamysantana@users.noreply.github.com>
2022-11-13 21:53:26 -0300

    Update Usuarioo01.java

--- tssviagens/model/Usuarioo01.java
+++ tssviagens/model/Usuarioo01.java
@@ -6,10 +6,8 @@ public class Usuarioo01 {
 
 		private int id_Usuario;
 		private String nome;
-		private String telefone;
 		private String email;
 		private String senha;
-		private Date dataCad;
 		public int getId_Usuario() {
 			return id_Usuario;
 		}
@@ -22,11 +20,7 @@ public class Usuarioo01 {
 		public void setNome(String nome) {
 			this.nome = nome;
 		}
-		public String getTelefone() {
-			return telefone;
-		}
-		public void setTelefone(String telefone) {
-			this.telefone = telefone;
+	
 		}
 		public String getEmail() {
 			return email;
@@ -39,12 +33,6 @@ public class Usuarioo01 {
 		}
 		public void setSenha(String senha) {
 			this.senha = senha;
-		}
-		public Date getDataCad() {
-			return dataCad;
-		}
-		public void setDataCad(Date dataCad) {
-			this.dataCad = dataCad;
 		}
 		
 	
